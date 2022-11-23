5785a0691cff4bdca306d180740a3ba956028e49
zhaozhenhang <zhaozhenhang@kuaishou.com>
2022-11-23 22:02:45 +0800

    feat: Data type conversion is handled through nix

--- src/ipc.rs
+++ src/ipc.rs
@@ -1,6 +1,5 @@
 use crate::errors::Errcode;
 use std::os::unix::io::RawFd;
-use nix::libc;
 use nix::sys::socket::{socketpair, AddressFamily, SockType, send, MsgFlags, recv, SockFlag};
 
 pub fn generate_socket_pair() -> Result<(RawFd, RawFd), Errcode> {
@@ -12,4 +11,23 @@ pub fn generate_socket_pair() -> Result<(RawFd, RawFd), Errcode> {
         Ok(res) => Ok(res),
         Err(_) => Err(Errcode::SocketError(0))
     }
+}
+
+pub fn send_boolean(fd: RawFd, boolean: bool) -> Result<(), Errcode> {
+    let data: [u8; 1] = [boolean.into()];
+    if let Err(e) = send(fd, &data, MsgFlags::empty()) {
+        log::error!("cannot send boolean through socket: {:?}", e);
+        return Err(Errcode::SocketError(1));
+    };
+    Ok(())
+}
+
+pub fn recv_boolean(fd: RawFd) -> Result<bool, Errcode> {
+    let mut data: [u8; 1] = [0];
+    if let Err(e) = recv(fd, &mut data, MsgFlags::empty()) {
+        log::error!("cannot receive boolean from socket: {:?}", e);
+        return Err(Errcode::SocketError(2));
+    }
+
+    Ok(data[0] == 1)
 }
