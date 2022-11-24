a8e6bfd7734b0b0f8dc52e9d9840e43bab6f2a0d
zhaozhenhang <zhaozhenhang@kuaishou.com>
2022-11-24 20:22:25 +0800

    feat: cleaning the restrictions

--- src/mounts.rs
+++ src/mounts.rs
@@ -1,29 +1,87 @@
 use crate::errors::Errcode;
-use nix::mount::{mount, umount2, MntFlags, MsFlags};
-use nix::unistd::{chdir, pivot_root};
-use rand::Rng;
-use std::fs::{create_dir_all, remove_dir};
+
 use std::path::PathBuf;
 
+//https://rust-lang-nursery.github.io/rust-cookbook/algorithms/randomness.html
+use rand::Rng;
+pub fn random_string(n: usize) -> String {
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                            0123456789";
+    let mut rng = rand::thread_rng();
+
+    let name: String = (0..n)
+        .map(|_| {
+            let idx = rng.gen_range(0..CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect();
+
+    name
+}
+
+pub fn unmount_path(path: &PathBuf) -> Result<(), Errcode>{
+    match umount2(path, MntFlags::MNT_DETACH){
+        Ok(_) => Ok(()),
+        Err(e) => {
+            log::error!("Unable to umount {}: {}", path.to_str().unwrap(), e);
+            Err(Errcode::MountsError(0))
+        }
+    }
+}
+
+pub fn delete_dir(path: &PathBuf) -> Result<(), Errcode>{
+    match remove_dir(path.as_path()){
+        Ok(_) => Ok(()),
+        Err(e) => {
+            log::error!("Unable to delete directory {}: {}", path.to_str().unwrap(), e);
+            Err(Errcode::MountsError(1))
+        }
+    }
+}
+
+use std::fs::create_dir_all;
+pub fn create_directory(path: &PathBuf) -> Result<(), Errcode>{
+    match create_dir_all(path) {
+        Err(e) => {
+            log::error!("Cannot create directory {}: {}", path.to_str().unwrap(), e);
+            Err(Errcode::MountsError(2))
+        },
+        Ok(_) => Ok(())
+    }
+}
+
+pub fn mount_directory(path: Option<&PathBuf>, mount_point: &PathBuf, flags: Vec<MsFlags>) -> Result<(), Errcode>{
+    let mut ms_flags = MsFlags::empty();
+    for f in flags.iter(){
+        ms_flags.insert(*f);
+    }
+    match mount::<PathBuf, PathBuf, PathBuf, PathBuf>(path, mount_point, None, ms_flags, None) {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            if let Some(p) = path{
+                log::error!("Cannot mount {} to {}: {}",
+                    p.to_str().unwrap(), mount_point.to_str().unwrap(), e);
+            }else{
+                log::error!("Cannot remount {}: {}",
+                    mount_point.to_str().unwrap(), e);
+            }
+            Err(Errcode::MountsError(3))
+        }
+    }
+}
+
+use std::fs::remove_dir;
+use nix::unistd::{pivot_root, chdir};
+use nix::mount::{mount, MsFlags, umount2, MntFlags};
 pub fn set_mount_point(mount_dir: &PathBuf) -> Result<(), Errcode> {
     log::debug!("Setting mount points ...");
-    mount_directory(
-        None,
-        &PathBuf::from("/"),
-        vec![MsFlags::MS_REC, MsFlags::MS_PRIVATE],
-    )?;
+    mount_directory(None, &PathBuf::from("/"), vec![MsFlags::MS_REC, MsFlags::MS_PRIVATE])?;
 
-    let new_root = PathBuf::from(format!("/tmp/mini-container.{}", random_string(12)));
-    log::debug!(
-        "Mounting temp directory {}",
-        new_root.as_path().to_str().unwrap()
-    );
+    let new_root = PathBuf::from(format!("/tmp/crabcan.{}", random_string(12)));
+    log::debug!("Mounting temp directory {}", new_root.as_path().to_str().unwrap());
     create_directory(&new_root)?;
-    mount_directory(
-        Some(&mount_dir),
-        &new_root,
-        vec![MsFlags::MS_BIND, MsFlags::MS_PRIVATE],
-    )?;
+    mount_directory(Some(&mount_dir), &new_root, vec![MsFlags::MS_BIND, MsFlags::MS_PRIVATE])?;
 
     log::debug!("Pivoting root");
     let old_root_tail = format!("oldroot.{}", random_string(6));
@@ -43,83 +101,7 @@ pub fn set_mount_point(mount_dir: &PathBuf) -> Result<(), Errcode> {
     Ok(())
 }
 
-pub fn clean_mounts(_root_path: &PathBuf) -> Result<(), Errcode> {
+pub fn clean_mounts(_rootpath: &PathBuf) -> Result<(), Errcode>{
+    //unmount_path(&rootpath)?;
     Ok(())
-}
-
-pub fn mount_directory(
-    path: Option<&PathBuf>,
-    mount_point: &PathBuf,
-    flags: Vec<MsFlags>,
-) -> Result<(), Errcode> {
-    let mut ms_flags = MsFlags::empty();
-    for f in flags.iter() {
-        ms_flags.insert(*f);
-    }
-    match mount::<PathBuf, PathBuf, PathBuf, PathBuf>(path, mount_point, None, ms_flags, None) {
-        Ok(_) => Ok(()),
-        Err(e) => {
-            if let Some(p) = path {
-                log::error!(
-                    "Cannot mount {} to {}: {}",
-                    p.to_str().unwrap(),
-                    mount_point.to_str().unwrap(),
-                    e
-                );
-            } else {
-                log::error!("Cannot remount {}: {}", mount_point.to_str().unwrap(), e);
-            }
-            Err(Errcode::MountsError(3))
-        }
-    }
-}
-
-pub fn random_string(n: usize) -> String {
-    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
-                            abcdefghijklmnopqrstuvwxyz\
-                            0123456789";
-    let mut rng = rand::thread_rng();
-
-    let name: String = (0..n)
-        .map(|_| {
-            let idx = rng.gen_range(0..CHARSET.len());
-            CHARSET[idx] as char
-        })
-        .collect();
-
-    name
-}
-
-pub fn create_directory(path: &PathBuf) -> Result<(), Errcode> {
-    match create_dir_all(path) {
-        Err(e) => {
-            log::error!("Cannot create directory {}: {}", path.to_str().unwrap(), e);
-            Err(Errcode::MountsError(2))
-        }
-        Ok(_) => Ok(()),
-    }
-}
-
-pub fn unmount_path(path: &PathBuf) -> Result<(), Errcode> {
-    match umount2(path, MntFlags::MNT_DETACH) {
-        Ok(_) => Ok(()),
-        Err(e) => {
-            log::error!("Unable to umount {}: {}", path.to_str().unwrap(), e);
-            Err(Errcode::MountsError(0))
-        }
-    }
-}
-
-pub fn delete_dir(path: &PathBuf) -> Result<(), Errcode> {
-    match remove_dir(path.as_path()) {
-        Ok(_) => Ok(()),
-        Err(e) => {
-            log::error!(
-                "Unable to delete directory {}: {}",
-                path.to_str().unwrap(),
-                e
-            );
-            Err(Errcode::MountsError(1))
-        }
-    }
 }
